314651f748396feaaa2ec0fd7cce391949bc5e7d
Mansi123tech <121337383+Mansi123tech@users.noreply.github.com>
2024-09-27 13:30:04 +0530

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,18 @@
+// var size = 140;
+// var w , r;
+// var l = prompt("Write here");
+// w = l.length;
+// r= size - w;
+// alert("written "+w+" left "+r );
+
+//cuting slice
+// var get  = prompt("enter tweet");
+// var show = prompt(get.slice(0,200));
+
+
+//DogToHumanAgeConvertor
+ var d = prompt("enter dog's age : - ");
+ alert("HumanAge = "+ ((d-2)*4+21));
+
+
+
